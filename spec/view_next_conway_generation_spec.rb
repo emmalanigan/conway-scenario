a00dcf66defe47eab6412f93e1c17967461b6163
kjdchapman <kjdchapman@gmail.com>
2022-10-31 15:02:05 +0000

    Change signature of living neighbours, start to design through TDD

--- spec/view_next_conway_generation_spec.rb
+++ spec/view_next_conway_generation_spec.rb
@@ -25,7 +25,8 @@ describe "viewing the next generation in conway's game of life" do
     expect(view_next_conway_generation(one_cell_grid)).to eq(empty_grid)
   end
 
-  it 'should return a non-empty grid when we implement a viable first generation' do
+
+  xit 'should return a non-empty grid when we implement a viable first generation' do
     viable_grid = [
       [:empty, :empty, :empty,:empty],
       [:empty, :alive, :alive,:empty],
@@ -36,13 +37,20 @@ describe "viewing the next generation in conway's game of life" do
   end
 end
 
+describe "how to calculate number of live neighbours in a conway grid" do
+  it "should tell me there is one live neighbour if one alive in a 2x1 grid" do
+    two_by_one_grid = [:empty, :alive]
+    number_of_neighbours = number_of_living_neigbours(two_by_one_grid,row: 0,column: 0)
+    expect(number_of_neighbours).to eq 1
+  end
+end
+
 def view_next_conway_generation(generation)
-  # How do I return a correct version of the next gen?
   next_generation = []
   generation.each do |row|
     future_row = []
     row.each do |cell|
-      future_row << conway_rule(cell,number_of_living_neigbours_next_to(cell))
+      future_row << conway_rule(cell,number_of_living_neigbours(cell,row:0,column:0))
     end
     next_generation << future_row
   end
@@ -52,5 +60,7 @@ end
 def conway_rule(cell,number_of_living_neigbours)
   :empty
 end
-def number_of_living_neigbours_next_to(cell)
+
+def number_of_living_neigbours(grid,row:,column:)
+  1
 end
