9b3936a238ffc0316ee33fb719b8a0891def465e
pinkkoala95 <emma.keeley@madetech.com>
2022-11-03 10:18:28 +0000

    refactor out some dead code elements for edges

--- game.rb
+++ game.rb
@@ -17,17 +17,14 @@ end
 def number_of_living_neigbours(grid, row:, column:)
     
   living_count = 0
-  neighbours_search = [row-1, row, row+1].product([column-1, column, column+1])
+  neighbours_search = [row-1, row, row+1].product([column-1, column, column+1]) 
 
     neighbours_search.each do |cell|
         current_row= cell[0]
         current_column= cell[1]
-        number_of_columns= grid.length
-        number_of_rows= grid[0].length
+        current_cell = [row, column]
         
-        if current_row < 0 || current_row > number_of_rows || current_column < 0 || current_column > number_of_columns
-            cell.delete(cell)
-        elsif grid.dig(current_row,current_column) == :alive && cell != [row, column]
+        if current_row>=0 && current_column>=0 && cell != current_cell && grid.dig(current_row,current_column) == :alive            
             living_count += 1
         end         
     end
